1337b4cd5fe8d8b86a393d5acd826365e3cd6199
Leeran Farin <leeran7@gmail.com>
2021-09-22 18:26:46 -0400

    finished city-search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
-
-function App() {
+function CityInput(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="input">
+      <label htmlFor="city">Enter a City: {props.code}</label>
+      <input id="city" className="input" name="city" onChange={props.changeCity}/>
     </div>
-  );
+  )
 }
-
-export default App;
+function City(props){
+  return (
+    <div className="item">
+      City: {props.code}
+    </div>
+  )
+}
+export default class App extends Component {
+  state= {
+    city: "",
+    zipCodes: []
+  }
+  updateCity = async (e) => {
+    await this.setState({ city: e.target.value });
+    await this.updateZipCodes();
+  }
+  updateZipCodes = async () => {
+    console.clear(); //clears screen to remove unwanted 404 errors when typing
+    await fetch(`http://ctp-zip-api.herokuapp.com/city/${this.state.city.toUpperCase()}`)
+      .then(async res => await res.json())
+      .then(async data => await this.setState({ zipCodes: data }))
+      .catch(error => {
+        console.log("Not a city..")
+      })
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>City Search</h2>
+        </div>
+        <CityInput changeCity={this.updateCity}/>
+        <div className="results">
+          <ul id="list">
+          
+          {
+            this.state.zipCodes.length > 0 ?
+                this.state.zipCodes.map(item => <li key={item}><City code={item}/></li>)
+                :
+                <li id="none">No Results</li>
+          }
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
